756ad0e72ccca75cb7131e2016fc6c120d6eaa2d
MattRayWeinert <43066793+MattRayWeinert@users.noreply.github.com>
2019-03-03 13:31:23 -0500

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,15 +5,13 @@ public class Main {
 	public static void main(String[] args)
 	{
 		Connect4 game = new Connect4();
+
+
 		
-		game.printRack();
-		game.userInput();
-		game.printRack();		
-		game.userInput();
-		game.printRack();
-		game.userInput();
-		game.printRack();
-		game.userInput();
-		game.printRack();
+		while(game.checkWin() != true)
+		{
+			game.userInput();
+			game.printRack();
+		}
 	}
 }
