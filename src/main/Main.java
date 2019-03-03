84aed6ff6c5de63428a6d107a29fe307dab0e78a
MattRayWeinert <43066793+MattRayWeinert@users.noreply.github.com>
2019-03-03 16:29:38 -0500

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,11 +6,20 @@ public class Main {
 	{
 		Connect4 game = new Connect4();
 
-
+		System.out.println("*** Player 1 will be given the number '1'");
+		System.out.println("*** Player 2 will be given the number '2'");
 		
 		while(game.checkWin() != true)
 		{
 			game.userInput();
+			game.printRack();
+			
+			if(game.checkWin() == true)
+			{
+				System.exit(0);
+			}
+			
+			game.userInput2();
 			game.printRack();
 		}
 	}
